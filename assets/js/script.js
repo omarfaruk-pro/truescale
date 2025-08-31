c916e720ec73c3fa6b73ff189d0026a174e63b35
omar faruk <omarf01851@gmail.com>
2025-08-31 22:26:10 +0600

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,53 @@ new InfiniteMarquee({
     duplicateCount: 2,
     pauseOnHover: true,
 });
-
+let body = document.querySelector('body');
 {
     let hamburger = document.querySelector('#hamburger');
     let menu = document.querySelector('#main_nav');
-    let body = document.querySelector('body');
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('active');
         body.classList.toggle('menu_active');
         hamburger.classList.toggle('active');
+    });
+}
+{
+    let openPopup = document.querySelectorAll('.open_popup');
+    let popup = document.querySelector('#contact_modal');
+    let closePopup = document.querySelector('#close_popup');
+    openPopup.forEach(item => {
+        item.addEventListener('click', () => {
+            popup.classList.add('active');
+        });
+    })
+
+    let contactForm = document.getElementById("contact_form");
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let form = e.target;
+        let formData = new FormData(form);
+        let formObj = Object.fromEntries(formData.entries());
+        console.log(formObj);
+        contactForm.reset();
+        popup.classList.add('thank');
+    })
+
+    closePopup.addEventListener('click', () => {
+        popup.classList.remove('active', 'thank');
+    });
+    body.addEventListener('click', (e) => {
+        if(e.target.classList.contains('modal')){
+            popup.classList.remove('active', 'thank');
+        }
+    });
+}
+{
+    let header = document.querySelector('header');
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset <= 0) {
+            header.classList.remove('scrolled');
+        } else {
+            header.classList.add('scrolled');
+        }
     });
 }
